e387ba40cbb577b3b3e5a13d8a253bfa8bcfbc9c
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2021-05-07 22:59:40 +0530

    completed

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ var btnTranslate = document.querySelector('#btn-translate');
 var txtInput = document.querySelector('#txt-input');
 var outputDiv = document.querySelector('#output');
 
-btnTranslate.addEventListener('click', () => {
-    outputDiv.innerText = 'aapsksd ' + txtInput.value;
-});
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+    return serverURL + "?text=" + text;
+}
+
+function errorHandler(error) {
+    alert("something error with the server");
+}
+
+function clickHandler() {
+
+    var inputText = txtInput.value; // taking input
+
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+        })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener('click', clickHandler);
 
